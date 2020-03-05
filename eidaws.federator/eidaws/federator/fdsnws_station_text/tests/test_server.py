2cb670cc52f37d38f9d111819da00ef3c3973cc5
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-03-05 14:51:42 +0100

    [TST] Add server tests; simplify client generation
    Simplify aiohttp test client creation by means of the pytest *Factories
    as fixtures* pattern. Add server tests in order to cover
    AIOHTTPKeywordParser functionalities.

--- eidaws.federator/eidaws/federator/fdsnws_station_text/tests/test_server.py
+++ eidaws.federator/eidaws/federator/fdsnws_station_text/tests/test_server.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
 import copy
 import pytest
-
-from aiohttp import web
 
 from eidaws.federator.fdsnws_station_text import create_app, SERVICE_ID
 from eidaws.federator.fdsnws_station_text.app import DEFAULT_CONFIG
@@ -12,35 +11,99 @@ from eidaws.federator.utils.misc import get_config
 from eidaws.utils.settings import FDSNWS_QUERY_METHOD_TOKEN
 
 
+@pytest.fixture
+def make_aiohttp_client(aiohttp_client):
+
+    config_dict = get_config(SERVICE_ID, defaults=DEFAULT_CONFIG)
+
+    async def _make_aiohttp_client(config_dict=config_dict):
+
+        app = create_app(config_dict)
+        return await aiohttp_client(app)
+
+    return _make_aiohttp_client
+
+
 class TestFDSNStationTextServer:
 
     PATH_QUERY = "/".join([FED_STATION_PATH_TEXT, FDSNWS_QUERY_METHOD_TOKEN])
 
-    @staticmethod
-    def create_server(*args, **kwargs):
-        return create_app(*args, **kwargs)
-
-    async def test_client_max_size(self, aiohttp_client):
+    async def test_client_max_size(self, make_aiohttp_client):
 
         client_max_size = 32
-
-        # reduce client_max_size
+        # avoid large request
         config_dict = copy.deepcopy(DEFAULT_CONFIG)
-        config_dict['client_max_size'] = client_max_size
+        config_dict["client_max_size"] = client_max_size
 
-        app = self.create_server(
+        client = await make_aiohttp_client(
             config_dict=get_config(SERVICE_ID, defaults=config_dict)
         )
 
-        client = await aiohttp_client(app)
-
-        data = (b'level=channel\n'
-                b'\n'
-                b'CH * * * 2020-01-01 2020-01-02')
+        data = b"level=channel\n" b"\n" b"CH * * * 2020-01-01 2020-01-02"
 
         assert client_max_size < len(data)
 
         resp = await client.post(self.PATH_QUERY, data=data)
 
         assert resp.status == 413
-        assert 'Request Entity Too Large' in await resp.text()
+        assert "Request Entity Too Large" in await resp.text()
+
+    async def test_get_with_strict_args_invalid(self, make_aiohttp_client):
+        client = await make_aiohttp_client()
+
+        resp = await client.get(self.PATH_QUERY, params={"foo": "bar"})
+
+        assert resp.status == 400
+        assert (
+            f"ValidationError: Invalid request query parameters: {{'foo'}}"
+            in await resp.text()
+        )
+
+    async def test_post_with_strict_args_invalid(self, make_aiohttp_client):
+        client = await make_aiohttp_client()
+
+        data = b"foo=bar\n\nNL HGN ?? * 2013-10-10 2013-10-11"
+        resp = await client.post(self.PATH_QUERY, data=data)
+
+        assert resp.status == 400
+        assert (
+            f"ValidationError: Invalid request query parameters: {{'foo'}}"
+            in await resp.text()
+        )
+
+    async def test_post_single_sncl(self, make_aiohttp_client):
+        config_dict = copy.deepcopy(DEFAULT_CONFIG)
+        config_dict[
+            "url_routing"
+        ] = "http://eida-federator.ethz.ch/eidaws/routing/1/query"
+        config_dict["pool_size"] = 1
+
+        client = await make_aiohttp_client(
+            config_dict=get_config(SERVICE_ID, defaults=config_dict)
+        )
+
+        data = b"NL HGN ?? * 2013-10-10 2013-10-11"
+        resp = await client.post(self.PATH_QUERY, data=data)
+
+        assert resp.status == 200
+
+        # XXX(damb): This is a workaround in order to avoid the message
+        # "Exception ignored in: <coroutine object ...>"
+        await asyncio.sleep(0.01)
+
+    async def test_post_empty(self, make_aiohttp_client):
+        client = await make_aiohttp_client()
+
+        data = b""
+        resp = await client.post(self.PATH_QUERY, data=data)
+
+        assert resp.status == 400
+
+    async def test_post_equal(self, make_aiohttp_client):
+        client = await make_aiohttp_client()
+
+        data = b"="
+        resp = await client.post(self.PATH_QUERY, data=data)
+
+        assert resp.status == 400
+        assert "ValidationError: RTFM :)." in await resp.text()
