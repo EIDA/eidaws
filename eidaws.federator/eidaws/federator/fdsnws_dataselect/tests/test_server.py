63565d80d9b1e7523a9f5623b6e7d199b28665ad
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-03-24 13:12:59 +0100

    [TST] Assert on unused routes

--- eidaws.federator/eidaws/federator/fdsnws_dataselect/tests/test_server.py
+++ eidaws.federator/eidaws/federator/fdsnws_dataselect/tests/test_server.py
@@ -85,6 +85,9 @@ class FakeServer:
         finally:
             self._responses[route].append(response)
 
+    def assert_no_unused_routes(self):
+        assert not self._responses
+
     def _find_response(self, request):
         def matches(route, request):
 
@@ -103,6 +106,8 @@ class FakeServer:
 
             try:
                 response = responses.pop(0)
+                if not responses:
+                    del self._responses[route]
             except IndexError:
                 response = None
             else:
@@ -112,11 +117,16 @@ class FakeServer:
 
 
 @pytest.fixture
-def make_aiohttp_client(aiohttp_client):
+def make_federated_eida(aiohttp_client):
 
     config_dict = get_config(SERVICE_ID, defaults=DEFAULT_CONFIG)
 
-    async def _make_aiohttp_client(
+    class FakedEndpointDictWrapper(dict):
+        def assert_no_unused_routes(self):
+            for faked_server in self.values():
+                faked_server.assert_no_unused_routes()
+
+    async def _make_federated_eida(
         config_dict=config_dict,
         mocked_routing_config={},
         mocked_endpoint_config={},
@@ -125,13 +135,14 @@ def make_aiohttp_client(aiohttp_client):
         assert len(mocked_routing_config.keys()) <= 1
 
         app = create_app(config_dict)
+        faked_routing = None
         # create mocked routing
         for host, mocked in mocked_routing_config.items():
-            fake_routing = FakeServer(host=host)
+            faked_routing = FakeServer(host=host)
             # mock routing responses
             for m in mocked:
-                fake_routing.add(*m)
-            info = await fake_routing.start()
+                faked_routing.add(*m)
+            info = await faked_routing.start()
 
             resolver = FakeResolver(info)
             routing_connector = aiohttp.TCPConnector(
@@ -141,8 +152,10 @@ def make_aiohttp_client(aiohttp_client):
 
         # create mocked endpoints
         faked_endpoint_info = {}
+        faked_endpoints = FakedEndpointDictWrapper()
         for host, mocked in mocked_endpoint_config.items():
             fake_endpoint = FakeServer(host=host)
+            faked_endpoints[host] = fake_endpoint
             # mock endpoint responses
             for m in mocked:
                 fake_endpoint.add(*m)
@@ -153,9 +166,9 @@ def make_aiohttp_client(aiohttp_client):
         endpoint_connector = aiohttp.TCPConnector(resolver=resolver, ssl=False)
         app["endpoint_http_conn_pool"] = endpoint_connector
 
-        return await aiohttp_client(app)
+        return await aiohttp_client(app), faked_routing, faked_endpoints
 
-    return _make_aiohttp_client
+    return _make_federated_eida
 
 
 @pytest.fixture(scope="session")
@@ -193,7 +206,7 @@ class TestFDSNDataselectServer:
         return get_config(SERVICE_ID, defaults=config_dict)
 
     async def test_get_no_route(
-        self, make_aiohttp_client, eidaws_routing_path_query
+        self, make_federated_eida, eidaws_routing_path_query
     ):
         method = "GET"
         mocked_routing = {
@@ -201,7 +214,7 @@ class TestFDSNDataselectServer:
                 (eidaws_routing_path_query, method, web.Response(status=204,),)
             ]
         }
-        client = await make_aiohttp_client(
+        client, faked_routing, faked_endpoints = await make_federated_eida(
             config_dict=self.get_default_config(),
             mocked_routing_config=mocked_routing,
         )
@@ -218,8 +231,10 @@ class TestFDSNDataselectServer:
         resp = await client.get(_PATH_QUERY, params=params)
         assert resp.status == 204
 
+        faked_routing.assert_no_unused_routes()
+
     async def test_get_no_data(
-        self, make_aiohttp_client, eidaws_routing_path_query
+        self, make_federated_eida, eidaws_routing_path_query
     ):
         method = "GET"
         mocked_routing = {
@@ -228,7 +243,7 @@ class TestFDSNDataselectServer:
                     eidaws_routing_path_query,
                     method,
                     web.Response(
-                        status=204,
+                        status=200,
                         text=(
                             "http://eida.ethz.ch/fdsnws/dataselect/1/query\n"
                             "CH FOO -- LHZ 2019-01-01T00:00:00 2019-01-05T00:00:00\n"
@@ -248,14 +263,10 @@ class TestFDSNDataselectServer:
             ]
         }
 
-        client = await make_aiohttp_client(
+        client, faked_routing, faked_endpoints = await make_federated_eida(
             config_dict=self.get_default_config(),
             mocked_routing_config=mocked_routing,
             mocked_endpoint_config=mocked_endpoints,
-        )
-        client = await make_aiohttp_client(
-            config_dict=self.get_default_config(),
-            mocked_routing_config=mocked_routing,
         )
 
         params = {
@@ -270,9 +281,12 @@ class TestFDSNDataselectServer:
         resp = await client.get(_PATH_QUERY, params=params)
         assert resp.status == 204
 
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
+
     async def test_get_single_stream_epoch(
         self,
-        make_aiohttp_client,
+        make_federated_eida,
         fdsnws_dataselect_content_type,
         load_data,
         eidaws_routing_path_query,
@@ -310,7 +324,7 @@ class TestFDSNDataselectServer:
             ]
         }
 
-        client = await make_aiohttp_client(
+        client, faked_routing, faked_endpoints = await make_federated_eida(
             config_dict=self.get_default_config(),
             mocked_routing_config=mocked_routing,
             mocked_endpoint_config=mocked_endpoints,
@@ -337,9 +351,12 @@ class TestFDSNDataselectServer:
             "CH.HASLI..LHZ.2019-01-01.2019-01-05T00:05:45"
         )
 
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
+
     async def test_get_multi_stream_epoch(
         self,
-        make_aiohttp_client,
+        make_federated_eida,
         fdsnws_dataselect_content_type,
         load_data,
         eidaws_routing_path_query,
@@ -388,7 +405,7 @@ class TestFDSNDataselectServer:
             ]
         }
 
-        client = await make_aiohttp_client(
+        client, faked_routing, faked_endpoints = await make_federated_eida(
             config_dict=self.get_default_config(),
             mocked_routing_config=mocked_routing,
             mocked_endpoint_config=mocked_endpoints,
@@ -413,9 +430,12 @@ class TestFDSNDataselectServer:
 
         assert data == load_data("CH.DAVOX,HASLI..LHZ.2019-01-01.2019-01-05")
 
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
+
     async def test_get_split_with_overlap(
         self,
-        make_aiohttp_client,
+        make_federated_eida,
         fdsnws_dataselect_content_type,
         load_data,
         eidaws_routing_path_query,
@@ -465,7 +485,7 @@ class TestFDSNDataselectServer:
             ]
         }
 
-        client = await make_aiohttp_client(
+        client, faked_routing, faked_endpoints = await make_federated_eida(
             config_dict=self.get_default_config(),
             mocked_routing_config=mocked_routing,
             mocked_endpoint_config=mocked_endpoints,
@@ -490,9 +510,12 @@ class TestFDSNDataselectServer:
 
         assert data == load_data("CH.HASLI..LHZ.2019-01-01.2019-01-10")
 
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
+
     async def test_get_split_without_overlap(
         self,
-        make_aiohttp_client,
+        make_federated_eida,
         fdsnws_dataselect_content_type,
         load_data,
         eidaws_routing_path_query,
@@ -544,7 +567,7 @@ class TestFDSNDataselectServer:
             ]
         }
 
-        client = await make_aiohttp_client(
+        client, faked_routing, faked_endpoints = await make_federated_eida(
             config_dict=self.get_default_config(),
             mocked_routing_config=mocked_routing,
             mocked_endpoint_config=mocked_endpoints,
@@ -571,9 +594,12 @@ class TestFDSNDataselectServer:
             "CH.HASLI..LHZ.2019-01-01.2019-01-01T00:10:00"
         )
 
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
+
     async def test_get_multi_split_with_overlap(
         self,
-        make_aiohttp_client,
+        make_federated_eida,
         fdsnws_dataselect_content_type,
         load_data,
         eidaws_routing_path_query,
@@ -650,7 +676,7 @@ class TestFDSNDataselectServer:
             ]
         }
 
-        client = await make_aiohttp_client(
+        client, faked_routing, faked_endpoints = await make_federated_eida(
             config_dict=self.get_default_config(),
             mocked_routing_config=mocked_routing,
             mocked_endpoint_config=mocked_endpoints,
@@ -674,3 +700,6 @@ class TestFDSNDataselectServer:
         data = await resp.read()
 
         assert data == load_data("CH.HASLI..LHZ.2019-01-01.2019-01-20")
+
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
