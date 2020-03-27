7816af9da2e73c324b2bcd96b84eb5d0febe23b9
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-03-27 22:10:35 +0100

    [TST] Rework fdsnws-station-xml related tests
    Make use of modularized testing facilities e.g. mock_federated_eida in
    order to simulate real federation. Also, enhance tests with real data.
    Besides tests are now executed for both the HTTP GET method and HTTP
    POST method.
    
    In order to test routing related server facilities make use of the mixin
    provided.

--- eidaws.federator/eidaws/federator/fdsnws_station_xml/tests/test_server.py
+++ eidaws.federator/eidaws/federator/fdsnws_station_xml/tests/test_server.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import asyncio
 import copy
+import functools
 import io
-import pathlib
 import pytest
 
+from aiohttp import web
 from lxml import etree
 
 from eidaws.federator.fdsnws_station_xml import create_app, SERVICE_ID
@@ -14,73 +14,116 @@ from eidaws.federator.utils.misc import get_config
 from eidaws.federator.fdsnws_station_xml.route import (
     FED_STATION_XML_PATH_QUERY,
 )
+from eidaws.federator.utils.pytest_plugin import (
+    fdsnws_station_xml_content_type,
+    eidaws_routing_path_query,
+    load_data,
+    make_federated_eida,
+)
+from eidaws.federator.utils.tests.server_mixin import _TestRoutingMixin
+from eidaws.utils.settings import FDSNWS_STATION_PATH_QUERY
 
 
-# TODO(damb): Check if both Redis and eida-federator.ethz.ch's Stationlite are
-# up and running. Else skip tests.
-
-
-_PATH_QUERY = FED_STATION_XML_PATH_QUERY
+# TODO(damb): Implement validate_station_xml(tree) in order to validate the
+# number of net, sta, cha objects. The function should return a list of tuples
+# with three elements.
 
 
 @pytest.fixture
-def make_aiohttp_client(aiohttp_client):
-
-    config_dict = get_config(SERVICE_ID, defaults=DEFAULT_CONFIG)
-
-    async def _make_aiohttp_client(config_dict=config_dict):
-
-        app = create_app(config_dict)
-        return await aiohttp_client(app)
-
-    return _make_aiohttp_client
-
-
-@pytest.fixture(scope="session")
-def xml_schema():
-    path_xsd = pathlib.Path(__file__).parent / "data" / "fdsn-station-1.0.xsd"
-    with open(path_xsd) as ifd:
-        xmlschema_doc = etree.parse(ifd)
-
+def xml_schema(load_data):
+    xsd = load_data("fdsn-station-1.0.xsd")
+    xmlschema_doc = etree.parse(io.BytesIO(xsd))
     return etree.XMLSchema(xmlschema_doc)
 
 
-@pytest.fixture(scope="session")
-def fdsnws_station_xml_content_type():
-    return "application/xml"
+class TestFDSNStationXMLServer(_TestRoutingMixin):
+    FED_PATH_QUERY = FED_STATION_XML_PATH_QUERY
+    PATH_QUERY = FDSNWS_STATION_PATH_QUERY
 
-
-class TestFDSNStationXMLServer:
     @staticmethod
     def get_default_config():
         config_dict = copy.deepcopy(DEFAULT_CONFIG)
-        config_dict[
-            "url_routing"
-        ] = "http://eida-federator.ethz.ch/eidaws/routing/1/query"
         config_dict["pool_size"] = 1
 
         return get_config(SERVICE_ID, defaults=config_dict)
 
-    async def test_get_single_sncl(
-        self, make_aiohttp_client, xml_schema, fdsnws_station_xml_content_type,
-    ):
-        client = await make_aiohttp_client(
-            config_dict=self.get_default_config()
-        )
+    @classmethod
+    def create_app(cls, config_dict=None):
 
-        _params = {
-            "net": "NL",
-            "sta": "HGN",
-            "loc": "??",
-            "cha": "*",
-            "start": "2013-11-10",
-            "end": "2013-11-11",
+        if config_dict is None:
+            config_dict = cls.get_default_config()
+
+        return functools.partial(create_app, config_dict)
+
+    @pytest.mark.parametrize(
+        "method,params_or_data",
+        [
+            (
+                "GET",
+                {
+                    "net": "NL",
+                    "start": "2013-11-10",
+                    "end": "2013-11-11",
+                    "level": "network",
+                    "format": "xml",
+                },
+            ),
+            (
+                "POST",
+                b"level=network\nformat=xml\nNL * * * 2013-11-10 2013-11-11",
+            ),
+        ],
+    )
+    async def test_single_sncl_level_network(
+        self,
+        make_federated_eida,
+        eidaws_routing_path_query,
+        fdsnws_station_xml_content_type,
+        xml_schema,
+        load_data,
+        method,
+        params_or_data,
+    ):
+        mocked_routing = {
+            "localhost": [
+                (
+                    eidaws_routing_path_query,
+                    method,
+                    web.Response(
+                        status=200,
+                        text=(
+                            "http://www.orfeus-eu.org/fdsnws/station/1/query\n"
+                            "NL * * * 2013-11-10T00:00:00 2013-11-11T00:00:00\n"
+                        ),
+                    ),
+                )
+            ]
+        }
+        mocked_endpoints = {
+            "www.orfeus-eu.org": [
+                (
+                    self.PATH_QUERY,
+                    "GET",
+                    web.Response(
+                        status=200,
+                        text=load_data(
+                            "NL....2013-11-10.2013-11-11.network",
+                            reader="read_text",
+                        ),
+                    ),
+                )
+            ]
         }
 
-        params = copy.deepcopy(_params)
-        params["level"] = "network"
+        client, faked_routing, faked_endpoints = await make_federated_eida(
+            self.create_app(),
+            mocked_routing_config=mocked_routing,
+            mocked_endpoint_config=mocked_endpoints,
+        )
 
-        resp = await client.get(_PATH_QUERY, params=params)
+        method = method.lower()
+        kwargs = {"params" if method == "get" else "data": params_or_data}
+        resp = await getattr(client, method)(self.FED_PATH_QUERY, **kwargs)
 
         assert resp.status == 200
         assert (
@@ -91,63 +134,80 @@ class TestFDSNStationXMLServer:
         xml = etree.parse(io.BytesIO(await resp.read()))
         assert xml_schema.validate(xml)
 
-        params = copy.deepcopy(_params)
-        params["level"] = "station"
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
 
-        resp = await client.get(_PATH_QUERY, params=params)
-
-        assert resp.status == 200
-        assert (
-            "Content-Type" in resp.headers
-            and resp.headers["Content-Type"] == fdsnws_station_xml_content_type
-        )
-
-        xml = etree.parse(io.BytesIO(await resp.read()))
-        assert xml_schema.validate(xml)
-
-        params = copy.deepcopy(_params)
-        params["level"] = "channel"
-
-        resp = await client.get(_PATH_QUERY, params=params)
-
-        assert resp.status == 200
-        assert (
-            "Content-Type" in resp.headers
-            and resp.headers["Content-Type"] == fdsnws_station_xml_content_type
-        )
-
-        xml = etree.parse(io.BytesIO(await resp.read()))
-        assert xml_schema.validate(xml)
-
-        params = copy.deepcopy(_params)
-        params["level"] = "response"
-
-        resp = await client.get(_PATH_QUERY, params=params)
-
-        assert resp.status == 200
-        assert (
-            "Content-Type" in resp.headers
-            and resp.headers["Content-Type"] == fdsnws_station_xml_content_type
-        )
-
-        xml = etree.parse(io.BytesIO(await resp.read()))
-        assert xml_schema.validate(xml)
-
-        # # XXX(damb): This is a workaround in order to avoid the message
-        # # "Exception ignored in: <coroutine object ...>"
-        await asyncio.sleep(0.01)
-
-    async def test_post_single_sncl(
-        self, make_aiohttp_client, xml_schema, fdsnws_station_xml_content_type
+    @pytest.mark.parametrize(
+        "method,params_or_data",
+        [
+            (
+                "GET",
+                {
+                    "net": "NL",
+                    "sta": "HGN",
+                    "start": "2013-11-10",
+                    "end": "2013-11-11",
+                    "level": "station",
+                    "format": "xml",
+                },
+            ),
+            (
+                "POST",
+                b"level=station\nformat=xml\nNL HGN * * 2013-11-10 2013-11-11",
+            ),
+        ],
+    )
+    async def test_single_sncl_level_station(
+        self,
+        make_federated_eida,
+        eidaws_routing_path_query,
+        fdsnws_station_xml_content_type,
+        xml_schema,
+        load_data,
+        method,
+        params_or_data,
     ):
-        client = await make_aiohttp_client(
-            config_dict=self.get_default_config()
+
+        mocked_routing = {
+            "localhost": [
+                (
+                    eidaws_routing_path_query,
+                    method,
+                    web.Response(
+                        status=200,
+                        text=(
+                            "http://www.orfeus-eu.org/fdsnws/station/1/query\n"
+                            "NL HGN * * 2013-11-10T00:00:00 2013-11-11T00:00:00\n"
+                        ),
+                    ),
+                )
+            ]
+        }
+        mocked_endpoints = {
+            "www.orfeus-eu.org": [
+                (
+                    self.PATH_QUERY,
+                    "GET",
+                    web.Response(
+                        status=200,
+                        text=load_data(
+                            "NL.HGN...2013-11-10.2013-11-11.station",
+                            reader="read_text",
+                        ),
+                    ),
+                )
+            ]
+        }
+
+        client, faked_routing, faked_endpoints = await make_federated_eida(
+            self.create_app(),
+            mocked_routing_config=mocked_routing,
+            mocked_endpoint_config=mocked_endpoints,
         )
 
-        sncl = b"NL HGN ?? * 2013-10-10 2013-10-11"
-
-        data = b"level=network\n" + sncl
-        resp = await client.post(_PATH_QUERY, data=data)
+        method = method.lower()
+        kwargs = {"params" if method == "get" else "data": params_or_data}
+        resp = await getattr(client, method)(self.FED_PATH_QUERY, **kwargs)
 
         assert resp.status == 200
         assert (
@@ -158,8 +218,80 @@ class TestFDSNStationXMLServer:
         xml = etree.parse(io.BytesIO(await resp.read()))
         assert xml_schema.validate(xml)
 
-        data = b"level=station\n" + sncl
-        resp = await client.post(_PATH_QUERY, data=data)
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
+
+    @pytest.mark.parametrize(
+        "method,params_or_data",
+        [
+            (
+                "GET",
+                {
+                    "net": "NL",
+                    "sta": "HGN",
+                    "cha": "BHZ",
+                    "start": "2013-11-10",
+                    "end": "2013-11-11",
+                    "level": "channel",
+                    "format": "xml",
+                },
+            ),
+            (
+                "POST",
+                b"level=channel\nformat=xml\nNL HGN * BHZ 2013-11-10 2013-11-11",
+            ),
+        ],
+    )
+    async def test_single_sncl_level_channel(
+        self,
+        make_federated_eida,
+        eidaws_routing_path_query,
+        fdsnws_station_xml_content_type,
+        xml_schema,
+        load_data,
+        method,
+        params_or_data,
+    ):
+        mocked_routing = {
+            "localhost": [
+                (
+                    eidaws_routing_path_query,
+                    method,
+                    web.Response(
+                        status=200,
+                        text=(
+                            "http://www.orfeus-eu.org/fdsnws/station/1/query\n"
+                            "NL HGN * BHZ 2013-11-10T00:00:00 2013-11-11T00:00:00\n"
+                        ),
+                    ),
+                )
+            ]
+        }
+        mocked_endpoints = {
+            "www.orfeus-eu.org": [
+                (
+                    self.PATH_QUERY,
+                    "GET",
+                    web.Response(
+                        status=200,
+                        text=load_data(
+                            "NL.HGN..BHZ.2013-11-10.2013-11-11.channel",
+                            reader="read_text",
+                        ),
+                    ),
+                )
+            ]
+        }
+
+        client, faked_routing, faked_endpoints = await make_federated_eida(
+            self.create_app(),
+            mocked_routing_config=mocked_routing,
+            mocked_endpoint_config=mocked_endpoints,
+        )
+
+        method = method.lower()
+        kwargs = {"params" if method == "get" else "data": params_or_data}
+        resp = await getattr(client, method)(self.FED_PATH_QUERY, **kwargs)
 
         assert resp.status == 200
         assert (
@@ -170,8 +302,80 @@ class TestFDSNStationXMLServer:
         xml = etree.parse(io.BytesIO(await resp.read()))
         assert xml_schema.validate(xml)
 
-        data = b"level=channel\n" + sncl
-        resp = await client.post(_PATH_QUERY, data=data)
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
+
+    @pytest.mark.parametrize(
+        "method,params_or_data",
+        [
+            (
+                "GET",
+                {
+                    "net": "NL",
+                    "sta": "HGN",
+                    "cha": "BHZ",
+                    "start": "2013-11-10",
+                    "end": "2013-11-11",
+                    "level": "response",
+                    "format": "xml",
+                },
+            ),
+            (
+                "POST",
+                b"level=response\nformat=xml\nNL HGN * BHZ 2013-11-10 2013-11-11",
+            ),
+        ],
+    )
+    async def test_single_sncl_level_response(
+        self,
+        make_federated_eida,
+        eidaws_routing_path_query,
+        fdsnws_station_xml_content_type,
+        xml_schema,
+        load_data,
+        method,
+        params_or_data,
+    ):
+        mocked_routing = {
+            "localhost": [
+                (
+                    eidaws_routing_path_query,
+                    method,
+                    web.Response(
+                        status=200,
+                        text=(
+                            "http://www.orfeus-eu.org/fdsnws/station/1/query\n"
+                            "NL HGN * BHZ 2013-11-10T00:00:00 2013-11-11T00:00:00\n"
+                        ),
+                    ),
+                )
+            ]
+        }
+        mocked_endpoints = {
+            "www.orfeus-eu.org": [
+                (
+                    self.PATH_QUERY,
+                    "GET",
+                    web.Response(
+                        status=200,
+                        text=load_data(
+                            "NL.HGN..BHZ.2013-11-10.2013-11-11.response",
+                            reader="read_text",
+                        ),
+                    ),
+                )
+            ]
+        }
+
+        client, faked_routing, faked_endpoints = await make_federated_eida(
+            self.create_app(),
+            mocked_routing_config=mocked_routing,
+            mocked_endpoint_config=mocked_endpoints,
+        )
+
+        method = method.lower()
+        kwargs = {"params" if method == "get" else "data": params_or_data}
+        resp = await getattr(client, method)(self.FED_PATH_QUERY, **kwargs)
 
         assert resp.status == 200
         assert (
@@ -182,18 +386,5 @@ class TestFDSNStationXMLServer:
         xml = etree.parse(io.BytesIO(await resp.read()))
         assert xml_schema.validate(xml)
 
-        data = b"level=response\n" + sncl
-        resp = await client.post(_PATH_QUERY, data=data)
-
-        assert resp.status == 200
-        assert (
-            "Content-Type" in resp.headers
-            and resp.headers["Content-Type"] == fdsnws_station_xml_content_type
-        )
-
-        xml = etree.parse(io.BytesIO(await resp.read()))
-        assert xml_schema.validate(xml)
-
-        # # XXX(damb): This is a workaround in order to avoid the message
-        # # "Exception ignored in: <coroutine object ...>"
-        await asyncio.sleep(0.01)
+        faked_routing.assert_no_unused_routes()
+        faked_endpoints.assert_no_unused_routes()
