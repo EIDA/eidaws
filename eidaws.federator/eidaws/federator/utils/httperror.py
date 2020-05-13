96df771af03d972d905c443cb0b3c0d4a6340548
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-05-13 22:28:42 +0200

    [STY] Black

--- eidaws.federator/eidaws/federator/utils/httperror.py
+++ eidaws.federator/eidaws/federator/utils/httperror.py
@@ -44,17 +44,17 @@ Service version:
 class FDSNHTTPError(HTTPException):
     """
     General HTTP error class for 5xx and 4xx errors for FDSN web services,
-    with error message according to standard. Needs to be subclassed for
+    with error message according to standards. Needs to be subclassed for
     individual error types.
-
     """
+
     status_code = 0
-    error_desc_short = ''
+    error_desc_short = ""
 
     DEFAULT_DOCUMENTATION_URI = FDSNWS_SERVICE_DOCUMENTATION_URI
-    DEFAULT_SERVICE_VERSION = ''
+    DEFAULT_SERVICE_VERSION = ""
 
-    CONTENT_TYPE = 'text/plain'
+    CONTENT_TYPE = "text/plain"
 
     @staticmethod
     def create(status_code, *args, **kwargs):
@@ -78,8 +78,14 @@ class FDSNHTTPError(HTTPException):
         else:
             return HTTPInternalServerError(*args, **kwargs)
 
-    def __init__(self, request, request_submitted=None, documentation_uri=None,
-                 service_version=None, error_desc_long=None):
+    def __init__(
+        self,
+        request,
+        request_submitted=None,
+        documentation_uri=None,
+        service_version=None,
+        error_desc_long=None,
+    ):
 
         documentation_uri = documentation_uri or self.DEFAULT_DOCUMENTATION_URI
         service_version = service_version or self.DEFAULT_SERVICE_VERSION
@@ -87,55 +93,72 @@ class FDSNHTTPError(HTTPException):
         request_submitted = request_submitted or datetime.datetime.utcnow()
 
         text = self.make_error_message(
-            self.status_code, self.error_desc_short, error_desc_long,
-            documentation_uri, request.url, request_submitted.isoformat(),
-            service_version)
+            self.status_code,
+            self.error_desc_short,
+            error_desc_long,
+            documentation_uri,
+            request.url,
+            request_submitted.isoformat(),
+            service_version,
+        )
 
         super().__init__(text=text, content_type=self.CONTENT_TYPE)
 
     @staticmethod
-    def make_error_message(status_code, description_short, description_long,
-                           documentation_uri, request_url, request_time,
-                           service_version):
+    def make_error_message(
+        status_code,
+        description_short,
+        description_long,
+        documentation_uri,
+        request_url,
+        request_time,
+        service_version,
+    ):
         """
         Return text of error message.
         """
 
         return ERROR_MESSAGE_TEMPLATE % (
-            status_code, description_short, description_long,
-            documentation_uri, request_url, request_time, service_version)
+            status_code,
+            description_short,
+            description_long,
+            documentation_uri,
+            request_url,
+            request_time,
+            service_version,
+        )
 
 
 class HTTPNoContent(FDSNHTTPError):
     status_code = 204
-    error_desc_short = ''
+    error_desc_short = ""
 
 
 class HTTPNotFound(FDSNHTTPError):
     status_code = 404
-    error_desc_short = ''
+    error_desc_short = ""
 
 
 class HTTPBadRequest(FDSNHTTPError):
     status_code = 400
-    error_desc_short = 'Bad request'
+    error_desc_short = "Bad request"
 
 
 class HTTPRequestEntityTooLarge(FDSNHTTPError):
     status_code = 413
-    error_desc_short = 'Request too large'
+    error_desc_short = "Request too large"
 
 
 class HTTPRequestURITooLong(FDSNHTTPError):
     status_code = 414
-    error_desc_short = 'Request URI too large'
+    error_desc_short = "Request URI too large"
 
 
 class HTTPInternalServerError(FDSNHTTPError):
     status_code = 500
-    error_desc_short = 'Internal server error'
+    error_desc_short = "Internal server error"
 
 
 class HTTPServiceUnavailable(FDSNHTTPError):
     status_code = 503
-    error_desc_short = 'Service temporarily unavailable'
+    error_desc_short = "Service temporarily unavailable"
