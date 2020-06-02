ffa6188e3a45b46410e781ec4b95c42357939ca1
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-06-02 15:59:52 +0200

    [BUG] Enable case-sensitive LIKE with SQLite

--- eidaws.stationlite/eidaws/stationlite/server/db.py
+++ eidaws.stationlite/eidaws/stationlite/server/db.py
@@ -2,8 +2,14 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
+from eidaws.stationlite.engine.db import configure_sqlite
+
 db = SQLAlchemy()
+
 
 def setup_db(app):
     db.init_app(app)
 
+    if app.config["SQLALCHEMY_DATABASE_URI"].startswith("sqlite"):
+        SQLITE_PRAGMAS = ["PRAGMA case_sensitive_like=on"]
+        configure_sqlite(SQLITE_PRAGMAS)
