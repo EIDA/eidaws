c42936d3288644842f624c9686aaa89b8a22307f
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-06-22 10:43:57 +0200

    [ENH] Implement fdsnws-availability format=geocsv
    Provide integration tests.

---
+++ eidaws.federator/eidaws/federator/fdsnws_availability/geocsv/process.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from eidaws.federator.fdsnws_availability.process import (
+    AvailabilityAsyncWorker,
+    AvailabilityRequestProcessor as _AvailabilityRequestProcessor,
+)
+from eidaws.federator.fdsnws_availability.geocsv.parser import (
+    AvailabilityQuerySchema,
+    AvailabilityExtentSchema,
+)
+from eidaws.federator.settings import (
+    FED_BASE_ID,
+    FED_AVAILABILITY_GEOCSV_SERVICE_ID,
+)
+from eidaws.utils.settings import (
+    FDSNWS_EXTENT_METHOD_TOKEN,
+    FDSNWS_QUERY_METHOD_TOKEN,
+)
+
+
+def _find_nth(string, substr, n):
+    if n == 0:
+        raise ValueError
+    elif n == 1:
+        return string.find(substr)
+    else:
+        return string.find(substr, _find_nth(string, substr, n - 1) + 1)
+
+
+class _AvailablityAsyncWorker(AvailabilityAsyncWorker):
+
+    SERVICE_ID = FED_AVAILABILITY_GEOCSV_SERVICE_ID
+
+    LOGGER = ".".join([FED_BASE_ID, SERVICE_ID, "worker"])
+
+    def _load(self, data):
+        if not data:
+            return None
+        # strip off header
+        return data[_find_nth(data, b"\n", 5) + 1 :]
+
+    def _dump(self, obj):
+        _sorted = sorted(obj)
+        return b"".join(obj[i] for i in _sorted)
+
+
+class _AvailablityQueryAsyncWorker(_AvailablityAsyncWorker):
+    QUERY_PARAM_SERIALIZER = AvailabilityQuerySchema
+
+
+class _AvailablityExtentAsyncWorker(_AvailablityAsyncWorker):
+    QUERY_PARAM_SERIALIZER = AvailabilityExtentSchema
+
+
+class AvailabilityRequestProcessor(_AvailabilityRequestProcessor):
+
+    SERVICE_ID = FED_AVAILABILITY_GEOCSV_SERVICE_ID
+    LOGGER = ".".join([FED_BASE_ID, SERVICE_ID, "process"])
+
+    @property
+    def content_type(self):
+        return "text/csv"
+
+    @property
+    def charset(self):
+        return "utf-8"
+
+    @property
+    def header(self):
+        def to_string(l):
+            return b"|".join(l)
+
+        header_fields = [
+            (b"#field_unit: unitless", b"#field_type: string", b"Network"),
+            (b"unitless", b"string", b"Station"),
+            (b"unitless", b"string", b"Location"),
+            (b"unitless", b"string", b"Channel"),
+        ]
+        if "quality" not in self.merge:
+            header_fields.append((b"unitless", b"string", b"Quality"))
+        if "samplerate" not in self.merge:
+            header_fields.append((b"hertz", b"float", b"SampleRate"))
+        header_fields.append((b"ISO_8601", b"datetime", b"Earliest"))
+        header_fields.append((b"ISO_8601", b"datetime", b"Latest"))
+        if self.RESOURCE_METHOD == FDSNWS_QUERY_METHOD_TOKEN:
+            if self.query_params.get("show") == "latestupdate":
+                header_fields.append((b"ISO_8601", b"datetime", b"Updated"))
+        elif self.RESOURCE_METHOD == FDSNWS_EXTENT_METHOD_TOKEN:
+            header_fields.append((b"ISO_8601", b"datetime", b"Updated"))
+            header_fields.append((b"unitless", b"integer", b"TimeSpans"))
+            header_fields.append((b"unitless", b"string", b"Restriction"))
+
+        header_units, header_types, header_names = zip(*header_fields)
+        header_units = to_string(header_units)
+        header_types = to_string(header_types)
+        header_names = to_string(header_names)
+
+        return b"\n".join(
+            [
+                b"#dataset: GeoCSV 2.0",
+                b"#delimiter: |",
+                header_units,
+                header_types,
+                header_names,
+            ]
+        )
+
+    async def _prepare_response(self, response):
+        response.content_type = self.content_type
+        response.charset = self.charset
+        response.headers["Content-Disposition"] = (
+            'inline; filename="'
+            + FED_BASE_ID.replace(".", "-")
+            + "-"
+            + datetime.datetime.utcnow().isoformat()
+            + '.txt"'
+        )
+        await response.prepare(self.request)
+        await response.write(self.header)
+        await response.write(b"\n")
+
+    def _create_worker(self, request, session, drain, lock=None, **kwargs):
+        return _AvailablityAsyncWorker(
+            self.request, session, drain, lock=lock,
+        )
+
+
+class AvailabilityQueryRequestProcessor(AvailabilityRequestProcessor):
+    RESOURCE_METHOD = FDSNWS_QUERY_METHOD_TOKEN
+
+    def _create_worker(self, request, session, drain, lock=None, **kwargs):
+        return _AvailablityQueryAsyncWorker(
+            self.request, session, drain, lock=lock,
+        )
+
+
+class AvailabilityExtentRequestProcessor(AvailabilityRequestProcessor):
+    RESOURCE_METHOD = FDSNWS_EXTENT_METHOD_TOKEN
+
+    def _create_worker(self, request, session, drain, lock=None, **kwargs):
+        return _AvailablityExtentAsyncWorker(
+            self.request, session, drain, lock=lock,
+        )
---
+++ eidaws.federator/eidaws/federator/fdsnws_availability/geocsv/tests/test_server.py
@@ -0,0 +1,157 @@
+import copy
+import functools
+import pytest
+
+from aiohttp import web
+
+from eidaws.federator.fdsnws_availability.geocsv import create_app, SERVICE_ID
+from eidaws.federator.fdsnws_availability.geocsv.app import DEFAULT_CONFIG
+from eidaws.federator.fdsnws_availability.geocsv.route import (
+    FED_AVAILABILITY_GEOCSV_PATH_EXTENT,
+    FED_AVAILABILITY_GEOCSV_PATH_QUERY,
+)
+from eidaws.federator.utils.misc import get_config
+from eidaws.federator.fdsnws_availability.tests.server_mixin import (
+    _TestAvailabilityExtentMixin,
+    _TestAvailabilityQueryMixin,
+    _TestAPIMixin,
+)
+from eidaws.federator.utils.pytest_plugin import (
+    fdsnws_availability_geocsv_content_type,
+    fdsnws_error_content_type,
+    eidaws_routing_path_query,
+    load_data,
+    make_federated_eida,
+    server_config,
+    cache_config,
+    tester,
+)
+from eidaws.federator.utils.tests.server_mixin import (
+    _TestCommonServerConfig,
+    _TestCORSMixin,
+    _TestKeywordParserMixin,
+    _TestRoutingMixin,
+    _TestServerBase,
+)
+from eidaws.utils.settings import (
+    FDSNWS_AVAILABILITY_PATH_EXTENT,
+    FDSNWS_AVAILABILITY_PATH_QUERY,
+)
+
+# required for _TestAvailabilityQueryMixin, _TestAvailabilityExtentMixin
+fdsnws_availability_content_type = fdsnws_availability_geocsv_content_type
+
+
+@pytest.fixture
+def content_tester(load_data):
+    async def _content_tester(resp, expected=None):
+        assert expected is not None
+        assert await resp.text() == load_data(expected, reader="read_text")
+
+    return _content_tester
+
+
+class _TestAvailabilityRequestServerMixin:
+    @staticmethod
+    def get_config(**kwargs):
+        config_dict = copy.deepcopy(DEFAULT_CONFIG)
+        config_dict.update(kwargs)
+
+        return get_config(SERVICE_ID, defaults=config_dict)
+
+    @classmethod
+    def create_app(cls, config_dict=None):
+
+        if config_dict is None:
+            config_dict = cls.get_config(**cls._DEFAULT_SERVER_CONFIG)
+
+        return functools.partial(create_app, config_dict)
+
+
+class TestFDSNAvailabilityQueryServer(
+    _TestCommonServerConfig,
+    _TestCORSMixin,
+    _TestKeywordParserMixin,
+    _TestRoutingMixin,
+    _TestAPIMixin,
+    _TestAvailabilityQueryMixin,
+    _TestAvailabilityRequestServerMixin,
+    _TestServerBase,
+):
+    FED_PATH_RESOURCE = FED_AVAILABILITY_GEOCSV_PATH_QUERY
+    PATH_RESOURCE = FDSNWS_AVAILABILITY_PATH_QUERY
+    SERVICE_ID = SERVICE_ID
+
+    _DEFAULT_SERVER_CONFIG = {"pool_size": 1}
+
+    @pytest.mark.parametrize(
+        "method,params_or_data",
+        [
+            ("GET", {"show": "foo"}),
+            ("POST", b"show=foo\nNET STA LOC CHA 2020-01-01 2020-01-02"),
+            ("GET", {"show": ""}),
+            ("POST", b"show=\nNET STA LOC CHA 2020-01-01 2020-01-02"),
+        ],
+    )
+    async def test_bad_request_query(
+        self,
+        make_federated_eida,
+        fdsnws_error_content_type,
+        method,
+        params_or_data,
+    ):
+        client, _, _ = await make_federated_eida(self.create_app())
+
+        method = method.lower()
+        kwargs = {"params" if method == "get" else "data": params_or_data}
+        resp = await getattr(client, method)(self.FED_PATH_RESOURCE, **kwargs)
+
+        assert resp.status == 400
+        assert "Error 400: Bad request" in await resp.text()
+        assert (
+            "Content-Type" in resp.headers
+            and resp.headers["Content-Type"] == fdsnws_error_content_type
+        )
+
+
+class TestFDSNAvailabilityExtentServer(
+    _TestCORSMixin,
+    _TestKeywordParserMixin,
+    _TestRoutingMixin,
+    _TestAPIMixin,
+    _TestAvailabilityExtentMixin,
+    _TestAvailabilityRequestServerMixin,
+    _TestServerBase,
+):
+    FED_PATH_RESOURCE = FED_AVAILABILITY_GEOCSV_PATH_EXTENT
+    PATH_RESOURCE = FDSNWS_AVAILABILITY_PATH_EXTENT
+    SERVICE_ID = SERVICE_ID
+
+    _DEFAULT_SERVER_CONFIG = {"pool_size": 1}
+
+    @pytest.mark.parametrize(
+        "method,params_or_data",
+        [
+            ("GET", {"merge": "overlap"}),
+            ("POST", b"merge=overlap\nNET STA LOC CHA 2020-01-01 2020-01-02"),
+        ],
+    )
+    async def test_bad_request_extent(
+        self,
+        make_federated_eida,
+        fdsnws_error_content_type,
+        method,
+        params_or_data,
+    ):
+        client, _, _ = await make_federated_eida(self.create_app())
+
+        method = method.lower()
+        kwargs = {"params" if method == "get" else "data": params_or_data}
+        resp = await getattr(client, method)(self.FED_PATH_RESOURCE, **kwargs)
+
+        assert resp.status == 400
+        assert "Error 400: Bad request" in await resp.text()
+        assert (
+            "Content-Type" in resp.headers
+            and resp.headers["Content-Type"] == fdsnws_error_content_type
+        )
