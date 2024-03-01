493b89ca532cf7b62750b174aaeca85b239f251c
Stefan Heimers <heimers@sed.ethz.ch>
2024-03-01 12:58:10 +0100

    made availability service work, FDSNWS_AVAILABILITY_PATH_QUERY was missing in eidaws/endpoint_proxy/route.py

--- eidaws.endpoint_proxy/eidaws/endpoint_proxy/route.py
+++ eidaws.endpoint_proxy/eidaws/endpoint_proxy/route.py
@@ -6,6 +6,7 @@ from eidaws.endpoint_proxy.view import RedirectView
 from eidaws.utils.settings import (
     FDSNWS_DATASELECT_PATH_QUERY,
     FDSNWS_STATION_PATH_QUERY,
+    FDSNWS_AVAILABILITY_PATH_QUERY,
     EIDAWS_WFCATALOG_PATH_QUERY,
 )
 
@@ -14,6 +15,7 @@ def setup_routes(app):
     routes = [
         web.view(FDSNWS_DATASELECT_PATH_QUERY, RedirectView),
         web.view(FDSNWS_STATION_PATH_QUERY, RedirectView),
+        web.view(FDSNWS_AVAILABILITY_PATH_QUERY, RedirectView),
         web.view(EIDAWS_WFCATALOG_PATH_QUERY, RedirectView),
     ]
 
